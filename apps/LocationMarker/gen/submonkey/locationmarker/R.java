d1d23bafa1582de364e48530b74d43ee82a42c7e
submonkey <hunt.if@gmail.com>
2013-09-07 20:02:56 +0700

    fix ui for location marker

--- apps/LocationMarker/gen/submonkey/locationmarker/R.java
+++ apps/LocationMarker/gen/submonkey/locationmarker/R.java
@@ -24,8 +24,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_refresh=0x7f080006;
-        public static final int action_save=0x7f080007;
+        public static final int action_exit=0x7f080006;
+        public static final int action_refresh=0x7f080007;
+        public static final int action_save=0x7f080008;
         public static final int action_settings=0x7f080005;
         public static final int area1=0x7f080002;
         public static final int area2=0x7f080003;
@@ -40,16 +41,17 @@ public final class R {
         public static final int marker=0x7f070000;
     }
     public static final class string {
-        public static final int action_refresh=0x7f050002;
-        public static final int action_save=0x7f050003;
+        public static final int action_exit=0x7f050002;
+        public static final int action_refresh=0x7f050003;
+        public static final int action_save=0x7f050004;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int area1=0x7f050007;
-        public static final int area2=0x7f050008;
-        public static final int area3=0x7f050009;
-        public static final int latitude=0x7f050005;
-        public static final int longitude=0x7f050006;
-        public static final int unknown=0x7f050004;
+        public static final int area1=0x7f050008;
+        public static final int area2=0x7f050009;
+        public static final int area3=0x7f05000a;
+        public static final int latitude=0x7f050006;
+        public static final int longitude=0x7f050007;
+        public static final int unknown=0x7f050005;
     }
     public static final class style {
         /** 
